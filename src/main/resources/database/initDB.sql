349fbaf6bed3e0240f33d33c877ce7a56861ffd8
Aleksandr Goncharov <teyqfo@gmail.com>
2022-05-30 11:16:24 +0300

    bugfix

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS student
     name       VARCHAR(30) not null,
     surname    VARCHAR(30) not null,
     patronymic VARCHAR(30) not null,
-    "group"    VARCHAR(30) not null,
+    group_name    VARCHAR(30) not null,
     birthday   DATE
 );
